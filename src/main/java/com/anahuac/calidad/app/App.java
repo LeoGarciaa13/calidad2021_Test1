8e3c0b738ea3c014a9dd2561017e26c107a66d31
LeoGarciaa13 <gleo2519@gmail.com>
2021-09-23 18:10:46 -0500

    Iniciando

---
+++ src/main/java/com/anahuac/calidad/app/App.java
@@ -0,0 +1,19 @@
+package com.anahuac.calidad.app;
+
+/**
+ * @author Leonardo Garcia
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Prueba Numero 1" );
+        String Intento = "Salio bien la prueba";
+        if(Intento.contains("bien"))
+        {
+            System.out.println("Prueba 1 realizada con exito");
+            System.out.println(Intento);
+        }
+    }
+}
